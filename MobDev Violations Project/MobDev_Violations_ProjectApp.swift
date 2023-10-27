6f80994cc31fa607a89e177ecc23ab02dd5c4848
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2023-10-26 19:44:08 -0600

    first commit locally

---
+++ MobDev Violations Project/MobDev_Violations_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobDev_Violations_ProjectApp.swift
+//  MobDev Violations Project
+//
+//  Created by octopus on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct MobDev_Violations_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
