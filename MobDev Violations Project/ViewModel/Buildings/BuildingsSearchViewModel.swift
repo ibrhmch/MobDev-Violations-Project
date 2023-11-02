591beed9e3e84b9b566ac314aa05945f34f00ece
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2023-11-02 15:42:50 -0600

    removed the getDetailsFunction

--- MobDev Violations Project/ViewModel/Buildings/BuildingsSearchViewModel.swift
+++ MobDev Violations Project/ViewModel/Buildings/BuildingsSearchViewModel.swift
@@ -11,28 +11,6 @@ import Foundation
 class BuildingsSearchViewModel: ObservableObject{
     @Published var buildingsFetched = false
     @Published var listOfBuildings: [Building]?
-    
-    func getBuildingByID(bin_id: String = "") async -> BuildingDetailsResponse? {
-        do {
-            guard let url = URL(string: "http://127.0.0.1:5000/get_building_data?bin_id=\(bin_id)") else {
-                print("Invalid URL")
-                return nil
-            }
-            
-            let (data, response) = try await URLSession.shared.data(from: url)
-            
-            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
-                print("Invalid Response or Status Code not 200")
-                return nil
-            }
-            
-            let building = try JSONDecoder().decode(BuildingDetailsResponse.self, from: data)
-            return building
-        } catch {
-            print("Network Request or Decoding Failed: \(error)")
-            return nil
-        }
-    }
 
     func setAllBuildings() async {
         do {
