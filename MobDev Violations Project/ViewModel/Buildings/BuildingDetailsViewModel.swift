cc20cc1448873561a56854d199d0539e47944d65
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2023-11-02 15:43:19 -0600

    created to support the BuildingDetailsViewCards

--- MobDev Violations Project/ViewModel/Buildings/BuildingDetailsViewModel.swift
+++ MobDev Violations Project/ViewModel/Buildings/BuildingDetailsViewModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+class BuildingDetailsViewModel {
+    
+    func getBuildingByID(bin_id: String = "") async -> BuildingDetailsResponse? {
+        do {
+            guard let url = URL(string: "http://127.0.0.1:5000/get_building_data?bin_id=\(bin_id)") else {
+                print("Invalid URL")
+                return nil
+            }
+            
+            let (data, response) = try await URLSession.shared.data(from: url)
+            
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                print("Invalid Response or Status Code not 200")
+                return nil
+            }
+            
+            let building = try JSONDecoder().decode(BuildingDetailsResponse.self, from: data)
+            return building
+        } catch {
+            print("Network Request or Decoding Failed: \(error)")
+            return nil
+        }
+    }
+}
