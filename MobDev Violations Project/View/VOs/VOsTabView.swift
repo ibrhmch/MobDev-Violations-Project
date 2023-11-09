3e7e6ab718be03569ffaacdaaae0db37b7408edb
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2023-11-09 15:07:31 -0700

    vo search tab view that displays a list of matching vos

--- MobDev Violations Project/View/VOs/VOsTabView.swift
+++ MobDev Violations Project/View/VOs/VOsTabView.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct VOsTabView: View {
+    @State private var searchText = ""
+    @State private var recentSearches = [String]()
+    @ObservedObject private var violationsVM = ViolationsSearchViewModel()
+    
+    
+    var searchResults: [ViolationOrder] {
+        if searchText.isEmpty {
+            return violationsVM.listOfViolations!
+        } else {
+            return (violationsVM.listOfViolations?.filter { $0.vo.contains(searchText.lowercased()) })!
+        }
+    }
+    
     var body: some View {
-        Text("VOs Tab")
+        NavigationView {
+            VStack {
+                HStack {
+                    TextField("Search Violations", text: $searchText)
+                        .onChange(of: searchText) {
+                            recentSearches.append(searchText)
+                            print(recentSearches)
+                            Task{
+                                await violationsVM.getSimilarViolations(searchText)
+                            }
+                        }
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+
+                    if !searchText.isEmpty {
+                        Button("Cancel") {
+                            searchText = ""
+                        }
+                        .padding(.trailing, 10)
+                    }
+                }
+                
+                if ((violationsVM.listOfViolations) != nil){
+                    List(searchResults, id: \.self) { result in
+                        Text(result.vo)
+                    }
+                }
+            }
+            .navigationBarTitle("Violations")
+        }
     }
 }
 
--- MobDev Violations Project/temp.playground/Contents.swift
+++ MobDev Violations Project/temp.playground/Contents.swift
@@ -1,42 +1,42 @@
 import UIKit
 
-//struct Building: Decodable, Hashable{
-//    let bin_id: String
-//    let address: String
-//}
-//
-//struct NumberOfNotices: Decodable {
-//    let bin: String
-//    let date: String
-//    let activenotices: Int
-//}
-//
-//struct NumberOfViolations: Decodable {
-//    let bin: String
-//    let date: String
-//    let activeviolations: Int
-//}
-//
-//struct NoticeOfViolations: Decodable {
-//    let bin: String
-//    let date: String
-//    let nov: String
-//    let status: Bool
-//}
-//
-//struct ViolationOrder: Decodable {
-//    let bin: String
-//    let date: String
-//    let status: Bool
-//    let vo: String
-//}
-//
-//struct BuildingDetailsResponse: Decodable {
-//    let violations: NumberOfViolations
-//    let notices: NumberOfNotices
-//    let listOfNotices: [NoticeOfViolations]
-//    let listOfViolations: [ViolationOrder]
-//}
+struct Building: Decodable, Hashable{
+    let bin_id: String
+    let address: String
+}
+
+struct NumberOfNotices: Decodable {
+    let bin: String
+    let date: String
+    let activenotices: Int
+}
+
+struct NumberOfViolations: Decodable {
+    let bin: String
+    let date: String
+    let activeviolations: Int
+}
+
+struct NoticeOfViolations: Decodable {
+    let bin: String
+    let date: String
+    let nov: String
+    let status: Bool
+}
+
+struct ViolationOrder: Decodable {
+    let bin_id: String
+    let date: String
+    let status: Bool
+    let vo: String
+}
+
+struct BuildingDetailsResponse: Decodable {
+    let violations: NumberOfViolations
+    let notices: NumberOfNotices
+    let listOfNotices: [NoticeOfViolations]
+    let listOfViolations: [ViolationOrder]
+}
 //
 //// -------------------------------------------------
 //
@@ -65,3 +65,28 @@ import UIKit
 //Task {
 //    await getBuildingByID(bin_id: "1023455")
 //}
+
+func getSimilarViolations(_ vo: String) async -> [ViolationOrder]? {
+    do {
+        guard let url = URL(string: "http://127.0.0.1:5000/get_similar_violations?vo=\(vo)") else {
+            print("Invalid URL")
+            return nil
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            print("Invalid Response or Status Code not 200")
+            return nil
+        }
+        
+        let violationsList = try JSONDecoder().decode([ViolationOrder].self, from: data)
+        return violationsList
+    } catch {
+        print("Network Request or Decoding Failed: \(error)")
+        return nil    }
+}
+
+Task{
+    await getSimilarViolations("E639")
+}
