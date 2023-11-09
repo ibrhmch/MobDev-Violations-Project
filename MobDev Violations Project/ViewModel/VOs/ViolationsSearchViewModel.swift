9b2c4b91e35d04ad4efff9e2256d269652a2adc5
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2023-11-09 15:07:09 -0700

    view model to fetch list of violations matching a search criteria

--- MobDev Violations Project/ViewModel/VOs/ViolationsSearchViewModel.swift
+++ MobDev Violations Project/ViewModel/VOs/ViolationsSearchViewModel.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+class ViolationsSearchViewModel: ObservableObject {
+    @Published var listOfViolations: [ViolationOrder]?
+    
+    
+    func getSimilarViolations(_ vo: String) async {
+        do {
+            guard let url = URL(string: "http://127.0.0.1:5000/get_similar_violations?vo=\(vo)") else {
+                print("Invalid URL")
+                self.listOfViolations = nil
+                return
+            }
+            
+            let (data, response) = try await URLSession.shared.data(from: url)
+            
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                print("Invalid Response or Status Code not 200")
+                self.listOfViolations = nil
+                return
+            }
+            
+            let violationsList = try JSONDecoder().decode([ViolationOrder].self, from: data)
+            self.listOfViolations = violationsList
+        } catch {
+            print("Network Request or Decoding Failed: \(error)")
+            self.listOfViolations = nil
+        }
+    }
+}
