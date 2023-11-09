c0bb9a089887000265927c1b4adc1bf7b8d081bc
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2023-11-09 12:38:43 -0700

    added search filtering

--- MobDev Violations Project/View/Buildings/BuildingsTabView.swift
+++ MobDev Violations Project/View/Buildings/BuildingsTabView.swift
@@ -12,21 +12,24 @@ import SwiftUI
 struct BuildingsTabView: View {
     @State private var searchText = ""
     @State private var recentSearches = [String]()
-    @State private var searchResults = [String]()
     @ObservedObject private var buildingsVM = BuildingsSearchViewModel()
     
-
+    
+    var searchResults: [Building] {
+        if searchText.isEmpty {
+            return buildingsVM.listOfBuildings!
+        } else {
+            return (buildingsVM.listOfBuildings?.filter { $0.bin_id.contains(searchText.lowercased()) })!
+        }
+    }
+    
     var body: some View {
         NavigationView {
             VStack {
                 HStack {
                     TextField("Search buildings", text: $searchText)
                         .onChange(of: searchText) {
-                            if !searchText.isEmpty {
-                                searchResults = generateRandomResults()
-                            } else {
-                                searchResults.removeAll()
-                            }
+
                         }
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .padding()
@@ -34,33 +37,13 @@ struct BuildingsTabView: View {
                     if !searchText.isEmpty {
                         Button("Cancel") {
                             searchText = ""
-                            searchResults.removeAll()
                         }
                         .padding(.trailing, 10)
                     }
                 }
-
-                if !recentSearches.isEmpty {
-                    List {
-                        Section(header: Text("Recent Searches")) {
-                            ForEach(recentSearches, id: \.self) { search in
-                                Text(search)
-                                    .onTapGesture {
-                                        searchText = search
-                                        searchResults = generateRandomResults()
-                                    }
-                            }
-                            .onDelete(perform: deleteItems)
-                        }
-                    }
-                }
-                
-                List(searchResults, id: \.self) { result in
-                    Text(result)
-                }
                 
                 if (buildingsVM.buildingsFetched){
-                    List(buildingsVM.listOfBuildings!, id: \.self) { result in
+                    List(searchResults, id: \.self) { result in
                         NavigationLink(destination: BuildingDetailCardView()) {
                             Text("\(result.bin_id)")
                         }
@@ -71,21 +54,6 @@ struct BuildingsTabView: View {
             .task{
                 await buildingsVM.setAllBuildings()
             }
-        }
-    }
-
-    func deleteItems(at offsets: IndexSet) {
-        recentSearches.remove(atOffsets: offsets)
-    }
-
-    func generateRandomResults() -> [String] {
-        if !recentSearches.contains(searchText) {
-            recentSearches.append(searchText)
-        }
-
-        let numberOfResults = Int.random(in: 2...3)
-        return (0..<numberOfResults).map { _ in
-            "Result \(Int.random(in: 1...100))"
         }
     }
 }
