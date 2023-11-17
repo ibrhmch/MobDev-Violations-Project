bf24faf1c781038d4623d365b1445e70283a9b7b
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2023-11-16 18:18:29 -0700

    created NOVSearchViewModel to support NOV Search

---
+++ MobDev Violations Project/ViewModel/NOVs/NOVSearchViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  NOVSearchViewModel.swift
+//  MobDev Violations Project
+//
+//  Created by octopus on 11/16/23.
+//
+
+import Foundation
+
+class NOVSearchViewModel: ObservableObject {
+    @Published var listOfNovs: [NoticeOfViolations]?
+    @Published var NOVsFetched: Bool = false
+    
+    func getSimilarViolations(_ nov: String) async {
+        do {
+            guard let url = URL(string: "http://127.0.0.1:5000/get_similar_nov?nov=\(nov)") else {
+                print("Invalid URL")
+                self.listOfNovs = nil
+                return
+            }
+            
+            let (data, response) = try await URLSession.shared.data(from: url)
+            
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                print("Invalid Response or Status Code not 200")
+                self.listOfNovs = nil
+                return
+            }
+            
+            let violationsList = try JSONDecoder().decode([NoticeOfViolations].self, from: data)
+            self.listOfNovs = violationsList
+            self.NOVsFetched = true
+        } catch {
+            print("Network Request or Decoding Failed: \(error)")
+            self.listOfNovs = nil
+        }
+    }
+}
