6aa15344246476c660da978b256fc4b6a0c8e638
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2023-11-16 18:43:40 -0700

    moved out the getDateFromString function to utility folder

---
+++ MobDev Violations Project/Utility/DateConverter.swift
@@ -0,0 +1,24 @@
+//
+//  DateConverter.swift
+//  MobDev Violations Project
+//
+//  Created by octopus on 11/16/23.
+//
+
+import Foundation
+
+func getDateFromString(_ dateString: String) -> String {
+    let inputFormatter = DateFormatter()
+    inputFormatter.dateFormat = "EEE, dd MMM yyyy HH:mm:ss 'GMT'"
+    inputFormatter.timeZone = TimeZone(abbreviation: "GMT")
+
+    guard let date = inputFormatter.date(from: dateString) else {
+        return "2022-01-01"
+    }
+    
+    let outputFormatter = DateFormatter()
+    outputFormatter.dateFormat = "yyyy-MM-dd" // or "dd MMM yyyy" for "10 Oct 2023"
+    outputFormatter.timeZone = TimeZone(abbreviation: "GMT") // Set the timezone if needed
+    
+    return outputFormatter.string(from: date)
+}
--- MobDev Violations Project/View/VOs/VOsDetailsView.swift
+++ MobDev Violations Project/View/VOs/VOsDetailsView.swift
@@ -83,22 +83,6 @@ struct VOsDetailsView: View {
         }
 }
 
-func getDateFromString(_ dateString: String) -> String {
-    let inputFormatter = DateFormatter()
-    inputFormatter.dateFormat = "EEE, dd MMM yyyy HH:mm:ss 'GMT'"
-    inputFormatter.timeZone = TimeZone(abbreviation: "GMT")
-
-    guard let date = inputFormatter.date(from: dateString) else {
-        return "2022-01-01"
-    }
-    
-    let outputFormatter = DateFormatter()
-    outputFormatter.dateFormat = "yyyy-MM-dd" // or "dd MMM yyyy" for "10 Oct 2023"
-    outputFormatter.timeZone = TimeZone(abbreviation: "GMT") // Set the timezone if needed
-    
-    return outputFormatter.string(from: date)
-}
-
 #Preview {
     VOsDetailsView(bin_id: "1091233", date: "Tue, 10 Oct 2023 14:14:25 GMT", status: true, vo: "LD123123")
 }
