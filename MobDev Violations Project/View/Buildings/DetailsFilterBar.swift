c53df05c129e313b168a6b30dcc13c62e77b154a
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2023-11-30 15:15:20 -0700

    reduce corner border radius to 15 to match styling

--- MobDev Violations Project/View/Buildings/DetailsFilterBar.swift
+++ MobDev Violations Project/View/Buildings/DetailsFilterBar.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct FilterBar: View {
-    @State private var selectedOption: Int = 0
+    @Binding var selectedFilterOption: Int
     private let buttonWidth: CGFloat = 100
     private let textHeight: CGFloat = 45
     private let buttonHeight: CGFloat = 40
@@ -22,8 +22,8 @@ struct FilterBar: View {
                 Rectangle()
                     .frame(width: buttonWidth, height: buttonHeight)
                     .foregroundColor(Color(red: 127/255, green: 151/255, blue: 204/255))
-                    .cornerRadius(20)
-                    .offset(x: CGFloat(selectedOption) * buttonWidth - buttonWidth)
+                    .cornerRadius(15)
+                    .offset(x: CGFloat(selectedFilterOption) * buttonWidth - buttonWidth)
                     .transition(.slide)
                     .padding(.horizontal, 5)
                 
@@ -31,7 +31,7 @@ struct FilterBar: View {
                 HStack(spacing: 0) {
                     Button(action: {
                         withAnimation {
-                            selectedOption = 0
+                            selectedFilterOption = 0
                         }
                     }) {
                         Text("Active")
@@ -42,7 +42,7 @@ struct FilterBar: View {
                     
                     Button(action: {
                         withAnimation {
-                            selectedOption = 1
+                            selectedFilterOption = 1
                         }
                     }) {
                         Text("All")
@@ -54,7 +54,7 @@ struct FilterBar: View {
                     
                     Button(action: {
                         withAnimation {
-                            selectedOption = 2
+                            selectedFilterOption = 2
                         }
                     }) {
                         Text("Dismissed")
@@ -65,9 +65,9 @@ struct FilterBar: View {
                 }
             }
             .background(Color(red: 216/255, green: 225/255, blue: 235/255))
-            .cornerRadius(25)
+            .cornerRadius(15)
             .overlay(
-                RoundedRectangle(cornerRadius: 25)
+                RoundedRectangle(cornerRadius: 15)
                     .stroke(Color.black.opacity(0.2), lineWidth: 1)
             )
             .frame(width: buttonWidth * 3)
@@ -75,11 +75,5 @@ struct FilterBar: View {
     
             Spacer()
         }
-        .padding()
     }
-}
-
-
-#Preview {
-    FilterBar()
 }
