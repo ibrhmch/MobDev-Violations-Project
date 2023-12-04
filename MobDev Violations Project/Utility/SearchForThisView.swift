3c0fb1b01b054da32fa7c4a34ee6a68290e42d31
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2023-12-04 00:53:21 -0700

    new view to show search for something message

---
+++ MobDev Violations Project/Utility/SearchForThisView.swift
@@ -0,0 +1,29 @@
+//
+//  SearchForThisView.swift
+//  MobDev Violations Project
+//
+//  Created by octopus on 12/4/23.
+//
+
+import SwiftUI
+
+struct SearchForThisView: View {
+    var message: String = "Please search for something"
+    
+    var body: some View {
+        VStack{
+            Image(systemName: "text.magnifyingglass")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+            Text(message)
+                .padding()
+                .font(.subheadline)
+        }
+        Spacer()
+    }
+}
+
+#Preview {
+    SearchForThisView(message: "Please search for something")
+}
