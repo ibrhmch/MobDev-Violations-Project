12a965f00ed2b84544fd29ae9b7699ae1638ea52
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2023-12-18 13:35:43 -0700

    experimenting with unit testing

---
+++ NY ViolationsTests/NY_ViolationsTests.swift
@@ -0,0 +1,16 @@
+//
+//  NY_ViolationsTests.swift
+//  NY ViolationsTests
+//
+//  Created by octopus on 12/18/23.
+//
+
+import XCTest
+
+final class NY_ViolationsTests: XCTestCase {
+
+    func testBuildingsSearchViewModel() {
+        let a = 1 + 2
+        XCTAssertEqual(a, 3)
+    }
+}
